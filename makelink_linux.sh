15f3d2d1249bf0ee40cf8ebd19a8280e71fa4fd6
hi-watana <hero95alive@gmail.com>
2017-10-09 22:37:00 +0900

    modify .gvimrc settings

--- makelink_linux.sh
+++ makelink_linux.sh
@@ -1,5 +1,6 @@
-for file in .gvimrc .tmux.conf .vimrc .gitconfig
+for file in .tmux.conf .vimrc .gitconfig
 do
 	ln -s $PWD/${file} $HOME/${file}
 done
 ln -s $PWD/.zshrc_linux $HOME/.zshrc
+ln -s /home/hiroki/initfiles/.gvimrc_linux $HOME/.gvimrc
