9bf05ea679f19203d0801befbb804e01ff482886
Hiroki Watanabe <hero95alive@gmail.com>
2018-07-14 10:42:27 +0900

    Modified.

--- makelink_macos.sh
+++ makelink_macos.sh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 for file in .tmux.conf .vimrc .ctags
-=======
-for file in .vimrc .gitconfig .ctags
->>>>>>> 6424cd662c6a969f0f6c82009b1d98c320f5769a
 do
 	ln -s $PWD/${file} $HOME/${file}
 done
